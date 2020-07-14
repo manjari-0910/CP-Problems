09e9cd6c286d1578b7bd7fc5c50ac315fd6e2336
meesalaaravind1 <meesalaaravind1@msitprogram.net>
2020-07-14 10:26:59 +0530

    Jul 14 10:26:24

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -5,6 +5,20 @@
 # distance(x1, y1, x2, y2), which you might call several times. Also, remember to use
 # almostEqual (instead of ==) when comparing floats.
 
+def slope(x1,y1,x2,y2):
+	return (y2-y1)/(x2-x1)
+
 def isrighttriangle(x1, y1, x2, y2, x3, y3):
 	# your code goes here
+	ab = slope(x1,y1,x2,y2)
+	bc = slope(x2,y2,x3,y3)
+	ca = slope(x3,y3,x1,y1)
+	if ab * bc == -1:
+		return True
+	elif bc*ca == -1:
+		return True
+	elif ab*ca == -1:
+		return True
+	else:
+		return False
 	pass
