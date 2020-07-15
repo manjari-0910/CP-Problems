794d93670b56e2f83a619a9e46903464967cf01f
meesalaaravind1 <meesalaaravind1@msitprogram.net>
2020-07-15 19:22:42 +0530

    15 Jul 19:22:49

--- 07-set_kth_digit-Python/set_kth_digit.py
+++ 07-set_kth_digit-Python/set_kth_digit.py
@@ -1,11 +1,22 @@
-# Write the function setKthDigit(n, k, d) that takes three integers -- n, k, and d 
-# where n is a possibly-negative int, k is a non-negative int, and d is a non-negative 
-# single digit (between 0 and 9 inclusive). This function returns the number n with 
-# the kth digit replaced with d. Counting starts at 0 and goes right-to-left, 
-# so the 0th digit is the rightmost digit. 
+# Write the function setKthDigit(n, k, d) that takes three integers -- n, k, and d
+# where n is a possibly-negative int, k is a non-negative int, and d is a non-negative
+# single digit (between 0 and 9 inclusive). This function returns the number n with
+# the kth digit replaced with d. Counting starts at 0 and goes right-to-left,
+# so the 0th digit is the rightmost digit.
 
 
 
 def fun_set_kth_digit(n, k, d):
-		return 0
+	num=list(str(n)[::-1])
+	try:
+		if num[k] in "0123456789":
+			num[k]=str(d)
+		elif(num[k] in "-"):
+			num[k]=str(d)
+			num.append("-")
+		return int(''.join(num[::-1]))
+	except:
+		num.append(str(d))
+		return int(''.join(num[::-1]))
+
 
