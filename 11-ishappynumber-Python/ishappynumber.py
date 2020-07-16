13b42c66301e435ebfb29acf7467fd12e33aca90
meesalaaravind1 <meesalaaravind1@msitprogram.net>
2020-07-16 13:24:02 +0530

    Jul 16 13:23:51

--- 11-ishappynumber-Python/ishappynumber.py
+++ 11-ishappynumber-Python/ishappynumber.py
@@ -1,10 +1,10 @@
 # ishappynumber(n) [10 pts]
-# A happy number is a number defined by the following process: Starting with any positive integer, replace the 
-# number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will 
-# stay ), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 
+# A happy number is a number defined by the following process: Starting with any positive integer, replace the
+# number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will
+# stay ), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1
 # are happy numbers.
 
-# Write the function ishappynumber(n) which takes a possibly-negative integer and returns True if it is happy and 
+# Write the function ishappynumber(n) which takes a possibly-negative integer and returns True if it is happy and
 # False otherwise. Note that all numbers less than 1 are not happy. Here are some test assertions for you:
 # assert(ishappynumber(-7) == False)
 # assert(ishappynumber(1) == True)
@@ -13,7 +13,27 @@
 # assert(ishappynumber(98) == False)
 # assert(ishappynumber(404) == True)
 # assert(ishappynumber(405) == False)
+import math
+
+def sqr(n):
+	return n**2
 
 def ishappynumber(n):
 	# your code goes here
+
+	if n<1:
+		return False
+	if n==1:
+		return True
+	while n!=1:
+		if len(str(n))==1:
+			break
+		l=list(str(n))
+		l=list(map(int,l))
+		l=list(map(sqr,l))
+		n=sum(l)
+	if n==1:
+		return True
+	else:
+		return False
 	pass
