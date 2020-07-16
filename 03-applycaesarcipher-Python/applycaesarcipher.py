c6f6ff0510c131ea6ac158568fe983d369b1f99d
meesalaaravind1 <meesalaaravind1@msitprogram.net>
2020-07-16 23:48:43 +0530

    Jul 16  23:48:43

--- 03-applycaesarcipher-Python/applycaesarcipher.py
+++ 03-applycaesarcipher-Python/applycaesarcipher.py
@@ -1,18 +1,21 @@
-# A Caesar Cipher is a simple cipher that works by shifting each letter in 
-# the given message by a certain number. For example, if we shift the message 
+# A Caesar Cipher is a simple cipher that works by shifting each letter in
+# the given message by a certain number. For example, if we shift the message
 # "We Attack At Dawn" by 1 letter, it becomes "Xf Buubdl Bu Ebxo"
-# Write the function applyCaesarCipher(message, shift) which shifts the given 
-# message by shift letters. You are guaranteed that message is a string, and 
-# that shift is an integer between -25 and 25. Capital letters should stay capital 
-# and lowercase letters should stay lowercase, and non-letter characters should not be changed. 
+# Write the function applyCaesarCipher(message, shift) which shifts the given
+# message by shift letters. You are guaranteed that message is a string, and
+# that shift is an integer between -25 and 25. Capital letters should stay capital
+# and lowercase letters should stay lowercase, and non-letter characters should not be changed.
 # Note that "Z" wraps around to "A". So, for example:
 # assert(applyCaesarCipher("We Attack At Dawn", 1) == "Xf Buubdl Bu Ebxo")
 # assert(applyCaesarCipher("zodiac", -2) == "xmbgya")
 
 
 def fun_applycaesarcipher(msg, shift):
-	return ""
-
-
-
-
+	s=""
+	for i in range (len(msg)):
+		letter=msg[i]
+		if (letter.islower()):
+			s+=chr((ord(letter)+s-97)%26+97)
+		else:
+			s+=chr((ord(letter)+s-65)%26+65)
+	return s
