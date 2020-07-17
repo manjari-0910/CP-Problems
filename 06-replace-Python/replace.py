011c470e4c6503825e3d0f6fc2d63af47398ae64
meesalaaravind1 <meesalaaravind1@msitprogram.net>
2020-07-17 20:11:37 +0530

    Jul 17  20:11:37

--- 06-replace-Python/replace.py
+++ 06-replace-Python/replace.py
@@ -3,13 +3,29 @@
 # replace(s1, s2, s3) that returns a string equal to
 # s1.replace(s2, s3), but again without calling s.replace().
 
+def multipletimes(s1,s2):
+	c=0
+	for i in range (len(s1)-len(s2)):
+		if s2 in s1[i:]:
+			c+=1
+	return c
 
 def fun_replace(s1, s2, s3):
 	try:
-		i1=s1.index(s2)
-		s=s1[:i1]
-		s+=s3
-		s+=s1[i1+len(s2):]
+		if multipletimes(s1,s2)==1:
+			i1=s1.index(s2)
+			s=s1[:i1]
+			s+=s3
+			s+=s1[i1+len(s2):]
+		else:
+			c=multipletimes(s1,s2)
+			i1=0
+			while c!=0:
+				i1=s1[i1+len(s2):].index(s2)
+				s=s1[:i1]
+				s+=s3
+				s+=s1[i1+len(s2):]
+				c-=1
 		return s
 	except:
 		return s1
