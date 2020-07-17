2e484cbb999c935a64f78756a2fa977e775a3762
meesalaaravind1 <meesalaaravind1@msitprogram.net>
2020-07-17 22:45:51 +0530

    Jul 17 22:45:39

--- 04-vowelscount-Java/vowelscount.java
+++ 04-vowelscount-Java/vowelscount.java
@@ -1,13 +1,20 @@
-// # Write the function vowelCount(s), that takes a string s, 
-// # and returns the number of vowels in s, ignoring case, 
-// # so "A" and "a" are both vowels. The vowels are "a", "e", "i", "o", and "u". 
+// # Write the function vowelCount(s), that takes a string s,
+// # and returns the number of vowels in s, ignoring case,
+// # so "A" and "a" are both vowels. The vowels are "a", "e", "i", "o", and "u".
 // # So, for example, ("Abc def!!! a? yzyzyz!") returns 3 (two a's and one e).
 
 
 class vowelscount {
 	public int fun_vowelscount(String s){
-		// your code goes here
-		return 0;
+		int count =0;
+		for (int i=0;i<s.length();i++){
+			if (s.charAt(i)=='a' || s.charAt(i)== 'e' || s.charAt(i)== 'i' || s.charAt(i)=='o' || s.charAt(i)=='u'){
+				count++;
+			}
+		}
+		return count;
 	}
-	
+	public static void main(String[] args) {
+
+	}
 }
