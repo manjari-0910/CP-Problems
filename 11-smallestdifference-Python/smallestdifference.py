78621570b4434a42fba245d5094c8c17f037bbd8
meesalaaravind1 <meesalaaravind1@msitprogram.net>
2020-07-17 20:41:12 +0530

    Jul 17  20:41:12

--- 11-smallestdifference-Python/smallestdifference.py
+++ 11-smallestdifference-Python/smallestdifference.py
@@ -6,11 +6,15 @@
 
 def smallestdifference(a):
 	# Your code goes here
-	min1=9223372036854775807
-	min2=9223372036854775807
-	for i in a:
-		if i <min1:
-			min2=min1
-			min1=i
-	return abs(min1-min2)
+	# min1=9223372036854775807
+	# min2=9223372036854775807
+	# for i in a:
+	# 	if i <min1:
+	# 		min2=min1
+	# 		min1=i
+	# return abs(min1-min2)
+	min1=min(a)
+	i=a.index(min1)
+	min2=min(a[i+1:])
+	return abs(min2-min1)
 	pass
