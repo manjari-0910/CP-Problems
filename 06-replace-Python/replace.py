ec8254c6511b65e4684bf5870e35e39ab820e031
meesalaaravind1 <meesalaaravind1@msitprogram.net>
2020-07-17 12:57:17 +0530

    last case not passed

--- 06-replace-Python/replace.py
+++ 06-replace-Python/replace.py
@@ -1,9 +1,15 @@
-# Without using the builtin method s.replace(), 
-# write its equivalent. Specifically, write the function 
-# replace(s1, s2, s3) that returns a string equal to 
+# Without using the builtin method s.replace(),
+# write its equivalent. Specifically, write the function
+# replace(s1, s2, s3) that returns a string equal to
 # s1.replace(s2, s3), but again without calling s.replace().
 
 
 def fun_replace(s1, s2, s3):
-	return s1
-
+	try:
+		i1=s1.index(s2)
+		s=s1[:i1]
+		s+=s3
+		s+=s1[i1+len(s2):]
+		return s
+	except:
+		return s1
