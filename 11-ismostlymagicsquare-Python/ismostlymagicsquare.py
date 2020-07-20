4615d711e50e046368982cf3e5d1695071ab81ab
meesalaaravind1 <meesalaaravind1@msitprogram.net>
2020-07-20 17:11:28 +0530

    Jul 20  17:11:28

--- 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
+++ 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
@@ -1,9 +1,9 @@
 # isMostlyMagicSquare(a) [15 pts]
-# Write the function isMostlyMagicSquare(a) that takes an 2d list of numbers, which you may assume is an NxN square 
+# Write the function isMostlyMagicSquare(a) that takes an 2d list of numbers, which you may assume is an NxN square
 # with N>0, and returns True if it is "mostly magic" and False otherwise, where a square is "mostly magic" if:
 # Each row, each column, and each of the 2 diagonals each sum to the same total.
-# A completely magic square has additional restrictions (such as not allowing duplicates, and only allowing numbers 
-# from 1 to N2), which we are not enforcing here, but which you can read about here. Note: any magic square is also 
+# A completely magic square has additional restrictions (such as not allowing duplicates, and only allowing numbers
+# from 1 to N2), which we are not enforcing here, but which you can read about here. Note: any magic square is also
 # a "mostly magic" square, including this sample magic square:
 # Read for more: https://en.wikipedia.org/wiki/Magic_square
 # Here is another mostly-magic square:
@@ -13,6 +13,12 @@
 #   [ 2, 1]]
 # Each row and each column add to 3, but one diagonal adds to 2 and the other to 4.
 
+import numpy as np
 def ismostlymagicsquare(a):
 	# Your code goes here
+	arr=np.matrix(a)
+	a1=arr.sum(axis=1)
+	a2=arr.sum(axis=0)
+	a3=np.trace(arr)
+	return min(a1)==max(a1) and min(a1)==a3 and min(a2)==max(a2) and min(a2)==a3
 	pass
