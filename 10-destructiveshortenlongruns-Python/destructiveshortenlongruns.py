e6a647e92d9748ba370fd7f23a1079247baf974f
meesalaaravind1 <meesalaaravind1@msitprogram.net>
2020-07-20 16:33:28 +0530

    Jul 20  16:33:27

--- 10-destructiveshortenlongruns-Python/destructiveshortenlongruns.py
+++ 10-destructiveshortenlongruns-Python/destructiveshortenlongruns.py
@@ -1,17 +1,25 @@
-# Here we will rewrite the previous function to be destructive. This version must not just call the nondestructive 
-# version from above and then clear and replace the values in the original list. Instead, you must traverse the list 
+# Here we will rewrite the previous function to be destructive. This version must not just call the nondestructive
+# version from above and then clear and replace the values in the original list. Instead, you must traverse the list
 # once and make the changes to the list as you go. With that in mind, write the destructive function shortenLongRuns(
-# L, k) that takes a list L and a positive integer k, and destructively modifies L by removing any values in L that 
-# would otherwise produce a run of k consecutive equal values in L. 
+# L, k) that takes a list L and a positive integer k, and destructively modifies L by removing any values in L that
+# would otherwise produce a run of k consecutive equal values in L.
 # For example:
-# L = [2, 3, 5, 5, 5, 3] 
+# L = [2, 3, 5, 5, 5, 3]
 # shortenLongRuns(L, 2)
 # assert(L == [2, 3, 5, 3])
-# And: 
+# And:
 # L = [2, 3, 5, 5, 5, 3]
 # shortenLongRuns(L, 3)
 # assert(L == [2, 3, 5, 5, 3])
 
 def destructiveshortenlongruns(L, k):
 	# Your code goes here
+	c=1
+	for i in range (len(L)-1):
+		if L[i]==L[i+1]:
+			c+=1
+		if c>k:
+			L.pop(i)
+			i=i-1
+	return L
 	pass
