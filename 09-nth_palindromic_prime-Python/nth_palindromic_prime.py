2e23143d286afe8f0c35ebdf12f417c18cb1a523
meesalaaravind1 <meesalaaravind1@msitprogram.net>
2020-07-20 13:52:13 +0530

    Jul 20  13:52:13

--- 09-nth_palindromic_prime-Python/nth_palindromic_prime.py
+++ 09-nth_palindromic_prime-Python/nth_palindromic_prime.py
@@ -1,10 +1,23 @@
-# Write the function fun_nth_palindromic_prime(n) that takes a non-negative int n 
-# and returns the nth palindromic Prime, which is a palidrome number such that 
-# it is also a prime. For example, 313 is a palindrome and it is prime 
+# Write the function fun_nth_palindromic_prime(n) that takes a non-negative int n
+# and returns the nth palindromic Prime, which is a palidrome number such that
+# it is also a prime. For example, 313 is a palindrome and it is prime
 # so 313 is an palindrome Prime. fun_nth_palindrome_prime(0) returns 2
 
+def isprime(n):
+	if n<=1:
+		return False
+	for i in range(2,(n//2)+1):
+		if n%i==0:
+			return False
+	return True
 
-
+def ispalin(n):
+	return str(n)==str(n)[::-1]
 
 def fun_nth_palindromic_prime(n):
-	return 0
+	num=1
+	while n>=0:
+		if isprime(num) and ispalin(num):
+			n-=1
+		num+=1
+	return num-1
