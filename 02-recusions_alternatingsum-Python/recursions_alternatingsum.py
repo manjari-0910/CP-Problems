710b5c3060f131cfe8a3696d4037f61d280469a6
meesalaaravind1 <meesalaaravind1@msitprogram.net>
2020-07-20 14:10:38 +0530

    Jul 20 14:10:27"

--- 02-recusions_alternatingsum-Python/recursions_alternatingsum.py
+++ 02-recusions_alternatingsum-Python/recursions_alternatingsum.py
@@ -1,8 +1,19 @@
-# Write the function alternatingSum(L) that takes a possibly-empty list of numbers, 
-# and returns the alternating sum of the list, where every other value is subtracted 
-# rather than added. For example: alternatingSum([1,2,3,4,5]) returns 1-2+3-4+5 
+# Write the function alternatingSum(L) that takes a possibly-empty list of numbers,
+# and returns the alternating sum of the list, where every other value is subtracted
+# rather than added. For example: alternatingSum([1,2,3,4,5]) returns 1-2+3-4+5
 # (that is, 3). If L is empty, return 0. You may not use loops/iteration in this problem.
 
 
-def fun_recursions_alternatingsum(l): 
-	return 0
+def fun_recursions_alternatingsum(l):
+	if (len(l)==0):
+		return 0
+	else:
+		pos=0
+		neg=0
+		for i in range(len(l)):
+			if i%2==0:
+				pos+=l[i]
+			else:
+				neg+=l[i]
+		return (pos-neg)
+	# return 0
