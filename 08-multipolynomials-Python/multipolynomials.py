87c9df04d683a5144c1b6424b57baa9e4e195671
meesalaaravind1 <meesalaaravind1@msitprogram.net>
2020-07-20 15:01:53 +0530

    Jul 20 15:01:33

--- 08-multipolynomials-Python/multipolynomials.py
+++ 08-multipolynomials-Python/multipolynomials.py
@@ -1,10 +1,14 @@
-# Background: we can represent a polynomial as a list of its coefficients. For example, [2, 3, 0, 4] could represent 
-# the polynomial 2x3 + 3x2 + 4. With this in mind, write the function multiplyPolynomials(p1, p2) which takes two 
-# lists representing polynomials as just described, and returns a third list representing the polynomial which is 
-# the product of the two. For example, multiplyPolynomials([2,0,3], [4,5]) represents the problem (2x**2 + 3)(4x + 
+# Background: we can represent a polynomial as a list of its coefficients. For example, [2, 3, 0, 4] could represent
+# the polynomial 2x3 + 3x2 + 4. With this in mind, write the function multiplyPolynomials(p1, p2) which takes two
+# lists representing polynomials as just described, and returns a third list representing the polynomial which is
+# the product of the two. For example, multiplyPolynomials([2,0,3], [4,5]) represents the problem (2x**2 + 3)(4x +
 # 5), and:    (2x**2 + 3)(4x + 5) = 8x**3 + 10x**2 + 12x + 15
 # And so this returns the list [8, 10, 12, 15].
+import numpy as np
 
 def multipolynomials(p1, p2):
 	# Your code goes here
+	pol1=np.poly1d(p1)
+	pol2=np.poly1d(p2)
+	return (list(np.polymul(pol1,pol2)))
 	pass
