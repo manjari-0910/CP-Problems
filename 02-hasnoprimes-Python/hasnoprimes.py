aaf896641e6c007dad3a337b15d1bfab6ae09c0f
meesalaaravind1 <meesalaaravind1@msitprogram.net>
2020-07-20 15:53:57 +0530

    Jul 20  15:53:57

--- 02-hasnoprimes-Python/hasnoprimes.py
+++ 02-hasnoprimes-Python/hasnoprimes.py
@@ -1,7 +1,28 @@
-# Write the function hasnoprimes(L) that takes a 2d list L of integers, 
+# Write the function hasnoprimes(L) that takes a 2d list L of integers,
 # and returns True if L does not contain any primes, and False otherwise.
 
+def isprime(n):
+	if n<=1:
+		return False
+	else:
+		for i in range(2,(n//2)+1):
+			if n%i==0:
+				return False
+		return True
+
+def fun_isrectangular(l):
+	t = len(l[0])
+	for i in l:
+		if len(i) != t:
+			return False
+	return True
 
 def fun_hasnoprimes(l):
+	if fun_isrectangular(l)==False:
+		return False
+	for i in range(len(l)):
+		for j in range(len(l[0])):
+			if isprime(l[i][j])==False:
+				return False
 	return True
 
