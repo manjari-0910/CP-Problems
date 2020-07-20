6e69acc059d605587447014493e110d413ae1cb2
meesalaaravind1 <meesalaaravind1@msitprogram.net>
2020-07-20 20:28:53 +0530

    Jul 20  20:28:53

--- 05-binarytreepractice-Python/binarytree.py
+++ 05-binarytreepractice-Python/binarytree.py
@@ -13,6 +13,20 @@ class BinaryTree(object):
         is in the tree, return
         False otherwise."""
         # Your code goes here
+        if find_val==self.root.value:
+            return True
+        if self.root.value is None:
+            return False
+        elif find_val<self.root.value:
+            if self.root.left is not None:
+                self.preorder_search(self.root.left,find_val)
+            else:
+                return False
+        else:
+            if self.root.right is not None:
+                self.preorder_search(self.root.right,find_val)
+            else:
+                return False
         pass
 
     def print_tree(self):
@@ -23,13 +37,25 @@ class BinaryTree(object):
         pass
 
     def preorder_search(self, start, find_val):
-        """Helper method - use this to create a 
+        """Helper method - use this to create a
         recursive search solution."""
         # Your code goes here
+        if find_val==start.value:
+            return True
+        elif find_val<=start.value:
+            if start.left is not None:
+                self.preorder_search(self.root.left,find_val)
+            else:
+                return False
+        else:
+            if start.right is not None:
+                self.preorder_search(self.root.right,find_val)
+            else:
+                return False
         pass
 
     def preorder_print(self, start, traversal):
-        """Helper method - use this to create a 
+        """Helper method - use this to create a
         recursive print solution."""
         # Your code goes here
         pass
