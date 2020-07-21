c5a9644354a09fc294b936e390d7bb7a5244e4f5
meesalaaravind1 <meesalaaravind1@msitprogram.net>
2020-07-21 14:53:39 +0530

    Jul 21  14:53:38

--- 06-recursion_secondlargest-Python/recursion_secondlargest.py
+++ 06-recursion_secondlargest-Python/recursion_secondlargest.py
@@ -1,8 +1,8 @@
 # Recursion-Only recursion_secondlargest(L) [15 pts]
-# Note: recall that you may not use sort, sorted, min, or max this week! With that in mind, write the function 
-# recursion_secondlargest(L) that takes a list of integers in any order and returns the second-largest value in the list. To 
-# be more precise, it returns the second value from the end if the list was sorted (though you do not need to sort 
-# it to solve this problem), so if the largest value occurs twice, you would return that value. Also, if there are 
+# Note: recall that you may not use sort, sorted, min, or max this week! With that in mind, write the function
+# recursion_secondlargest(L) that takes a list of integers in any order and returns the second-largest value in the list. To
+# be more precise, it returns the second value from the end if the list was sorted (though you do not need to sort
+# it to solve this problem), so if the largest value occurs twice, you would return that value. Also, if there are
 # fewer than 2 values in the list, return None. Here are some test cases:
 # assert(recursion_secondlargest([1,2,3,4,5]) == 4)
 # assert(recursion_secondlargest([4,3]) == 3)
@@ -10,9 +10,22 @@
 # assert(recursion_secondlargest([-3,-4]) == -4)
 # assert(recursion_secondlargest([4]) == None)
 # assert(recursion_secondlargest([ ]) == None)
-# Again, you do not need to sort the list. We didn't sort it in our sample solution. We just tracked the two largest 
+# Again, you do not need to sort the list. We didn't sort it in our sample solution. We just tracked the two largest
 # values as we recursively traversed the list. Also, you may not use loops/iteration in this problem
 
 def recursion_secondlargest(L):
 	# Your code goes here
+	if len(l)<2:
+		return None
+	return secondsmall(L,0,2147483647,2147483647)
 	pass
+
+def secondsmall(l,i,n1,n2):
+	if i==len(l):
+		return n2
+	else:
+		if l[i]<n1:
+			n2=n1
+			n1=l[i]
+		i+=1
+		return secondsmall(l,i,n1,n2)
