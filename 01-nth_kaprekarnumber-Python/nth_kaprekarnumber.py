d53a8eb4ae9eddd5916fb8743568e9530711ed84
meesalaaravind1 <meesalaaravind1@msitprogram.net>
2020-07-24 23:01:54 +0530

    Jul 24  23:01:53

--- 01-nth_kaprekarnumber-Python/nth_kaprekarnumber.py
+++ 01-nth_kaprekarnumber-Python/nth_kaprekarnumber.py
@@ -1,13 +1,26 @@
-# Background: a Kaprekar number is a non-negative integer, the representation of whose square 
-# can be split into two possibly-different-length parts (where the right part is not zero) 
-# that add up to the original number again. For instance, 45 is a Kaprekar number, because 
-# 45**2 = 2025 and 20+25 = 45. You can read more about Kaprekar numbers here. The first several 
-# Kaprekar numbers are: 1, 9, 45, 55, 99, 297, 703, 999 , 2223, 2728,... 
-# With this in mind, write the function nthKaprekarNumber(n) that takes a non-negative int n 
+# Background: a Kaprekar number is a non-negative integer, the representation of whose square
+# can be split into two possibly-different-length parts (where the right part is not zero)
+# that add up to the original number again. For instance, 45 is a Kaprekar number, because
+# 45**2 = 2025 and 20+25 = 45. You can read more about Kaprekar numbers here. The first several
+# Kaprekar numbers are: 1, 9, 45, 55, 99, 297, 703, 999 , 2223, 2728,...
+# With this in mind, write the function nthKaprekarNumber(n) that takes a non-negative int n
 # and returns the nth Kaprekar number, where as usual we start counting at n==0.
 
 
 import math
 
+def iskaprekar(i):
+    n=str(i**2)
+    if len(n)>1:
+        if i==(int(str(n[:len(n)//2]))+int(str(n[len(n)//2:]))):
+            return True
+    return False
+
 def fun_nth_kaprekarnumber(n):
-    return 1;
+    i=1
+    count=1
+    while(count<n):
+        if iskaprekar(i):
+            count+=1
+        i+=1
+    return i
