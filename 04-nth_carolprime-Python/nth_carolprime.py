0c6d13db4787882dfb75059e22a9c1c409a91ff9
meesalaaravind1 <meesalaaravind1@msitprogram.net>
2020-07-24 13:37:18 +0530

    Jul 24  13:37:18

--- 04-nth_carolprime-Python/nth_carolprime.py
+++ 04-nth_carolprime-Python/nth_carolprime.py
@@ -1,13 +1,37 @@
-# Write the function nthCarolPrime(n), which takes a non-negative int and returns the nth Carol Prime, 
-# which is a prime number of the form ((2**k - 1)**2 - 2) for some value positive int k. For example, 
-# if k equals 3, ((2**3 - 1)**2 -2) equals 47, which is prime, and so 47 is a Carol Prime. 
-# The first several Carol primes are: 7, 47, 223, 959, 3967, 16127, 65023, 261119, 1046527,... As such, 
+# Write the function nthCarolPrime(n), which takes a non-negative int and returns the nth Carol Prime,
+# which is a prime number of the form ((2**k - 1)**2 - 2) for some value positive int k. For example,
+# if k equals 3, ((2**3 - 1)**2 -2) equals 47, which is prime, and so 47 is a Carol Prime.
+# The first several Carol primes are: 7, 47, 223, 959, 3967, 16127, 65023, 261119, 1046527,... As such,
 # nthCarolPrime(0) returns 7.
-# Note: You must use a reasonably efficient approach that quickly works up to n==9, which 
-# will return a 12-digit answer! In particular, this means you cannot just edit isPrime. 
-# Hint: you may need to generate only Carol numbers, and then test those as you go 
+# Note: You must use a reasonably efficient approach that quickly works up to n==9, which
+# will return a 12-digit answer! In particular, this means you cannot just edit isPrime.
+# Hint: you may need to generate only Carol numbers, and then test those as you go
 # for primality (and you may need to think about that hint for a while for it to make sense!).
 
+def isprime(n) :
+
+    # Corner cases
+    if (n <= 1) :
+        return False
+    if (n <= 3) :
+        return True
+
+    # This is checked so that we can skip
+    # middle five numbers in below loop
+    if (n % 2 == 0 or n % 3 == 0) :
+        return False
+
+    i = 5
+    while(i * i <= n) :
+        if (n % i == 0 or n % (i + 2) == 0) :
+            return False
+        i = i + 6
+
+    return True
 
 def fun_nth_carolprime(n):
-    return 0
+    li=[]
+    for k in range(2,n):
+        if isprime((2**k - 1)**2 - 2):
+            li.append(((2**k - 1)**2 - 2))
+    return li[-1]
