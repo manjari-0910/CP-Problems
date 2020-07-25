b9b3201044d983b347867c8b5ee8f121c74f7169
meesalaaravind1 <meesalaaravind1@msitprogram.net>
2020-07-25 16:22:44 +0530

    Jul 25 16:22:58

--- 05-longestcommonsubstring-Python/longestcommonsubstring.py
+++ 05-longestcommonsubstring-Python/longestcommonsubstring.py
@@ -1,12 +1,27 @@
 # longestCommonSubstring(s1, s2)[20 pts]
-# Write the function, longestCommonSubstring(s1, s2), that takes two possibly-empty strings and returns the longest 
+# Write the function, longestCommonSubstring(s1, s2), that takes two possibly-empty strings and returns the longest
 # string that occurs in both strings (and returns the empty string if either string is empty). For example:
 # longestCommonSubstring("abcdef", "abqrcdest") returns "cde"
 # longestCommonSubstring("abcdef", "ghi") returns "" (the empty string)
-# If there are two or more longest common substrings, return the lexicographically smaller one (ie, just use "<" to 
+# If there are two or more longest common substrings, return the lexicographically smaller one (ie, just use "<" to
 # compare the strings). So, for example:
 # longestCommonSubstring("abcABC", "zzabZZAB") returns "AB" and not "ab"
 
 def longestcommonsubstring(s1, s2):
     # Yourcode goes here
+    i=0
+    a=s1
+    li=[]
+    while i<=len(a):
+        j=0
+        while(i+j<=len(a)):
+            if len(a[i:i+j])>0:
+                li.append(a[i:i+j])
+            j+=1
+        i+=1
+    l2 = sorted(li, key=len)
+    for i in l2[::-1]:
+        if i in s2:
+            return i
+    return ''
     pass
