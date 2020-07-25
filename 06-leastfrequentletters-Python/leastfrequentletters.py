b75de062a8d97cec431b645eaa23591d618a3757
meesalaaravind1 <meesalaaravind1@msitprogram.net>
2020-07-25 15:34:14 +0530

    Jul 25  15:34:14

--- 06-leastfrequentletters-Python/leastfrequentletters.py
+++ 06-leastfrequentletters-Python/leastfrequentletters.py
@@ -1,12 +1,26 @@
 # leastFrequentLetters(s) [20 pts]
-# Write the function leastFrequentLetters(s), that takes a string s, and ignoring case (so "A" and "a" are treated 
-# the same), returns a lowercase string containing the least-frequent alphabetic letters that occur in s, each 
+# Write the function leastFrequentLetters(s), that takes a string s, and ignoring case (so "A" and "a" are treated
+# the same), returns a lowercase string containing the least-frequent alphabetic letters that occur in s, each
 # included only once in the result and then in alphabetic order. So:
-# leastFrequentLetters("aDq efQ? FB'daf!!!") 
-# returns "be". Note that digits, punctuation, and whitespace are not letters! Also note that seeing as we have not 
-# yet covered lists, sets, maps, or efficiency, you are not expected to write the most efficient solution. Finally, 
+# leastFrequentLetters("aDq efQ? FB'daf!!!")
+# returns "be". Note that digits, punctuation, and whitespace are not letters! Also note that seeing as we have not
+# yet covered lists, sets, maps, or efficiency, you are not expected to write the most efficient solution. Finally,
 # if s does not contain any alphabetic characters, the result should be the empty string ("")
 
 def leastfrequentletters(s):
 	# Your code goes here
+	s=s.lower()
+	w=set(s)
+	d={}
+	for i in w:
+		# if i!='':
+		d[i]=s.count(i)
+	keys=list(d.keys())
+	vals=list(d.values())
+	li=[]
+	for i in range(len(vals)):
+		if vals[i]==min(vals):
+			li.append(keys[i])
+	li.sort()
+	return ''.join(li).strip()
 	pass
