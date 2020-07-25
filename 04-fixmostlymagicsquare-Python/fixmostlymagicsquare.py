dd80bca3ca7512e25f796e2c0728c867ae15ab50
meesalaaravind1 <meesalaaravind1@msitprogram.net>
2020-07-25 23:18:01 +0530

    Jul 25  23:18:01

--- 04-fixmostlymagicsquare-Python/fixmostlymagicsquare.py
+++ 04-fixmostlymagicsquare-Python/fixmostlymagicsquare.py
@@ -4,28 +4,35 @@
 # exercise, we assume we have just such a list L, and your task is to find and fix that change. So, given the list
 # L, return a new list M such that M is the same as L, only with exactly one value changed, and M is a mostly magic
 # square.
-
+import numpy as np
 
 def fixmostlymagicsquare(L):
 	pass
-	r1=r2=r3=c1=c2=c3=0
+	l1=[]
+	l2=[]
+	s1=0
+	s2=0
+	row=0
+	col=0
+	for i in L:
+		l1.append(i)
+	for i in range(len(l1)):
+		if l1.count(l1[i])==1:
+			row=i
+			s2=l1[i]
+		else:
+			s1=l1[i]
+	trans=np.array(L).T
+	for i in trans:
+		l2.append(i)
+	for i in range(len(l2)):
+		if np.any(l2.count(l2[i])==1):
+			col=i
+	if s2>s1:
+		diff=s2-s1
+	else:
+		diff=s1-s2
+	L[row][col]=l[row][col]-diff
+	return L
 
-	for i in range (len(L)):
-		if i==0:
-			r1=sum(l[i])
-		if i==1:
-			r2=sum(l[i])
-		if i==2:
-			r3=sum(l[i])
-	for i in range(len(L[0])):
-		if i==0:
-			c1=L[0][0]+L[0][1]+L[0][2]
-		if i==1:
-			c2=L[1][0]+L[1][1]+L[1][2]
-		if i==2:
-			c3=L[2][0]+L[2][1]+L[2][2]
-	if r1==r2 and r1!=r3:
-		if c1==c2 and c1!=c3:
-			a=r1-r3
-			L[2][2]
 	# Your code goes here
